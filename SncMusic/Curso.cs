9080ec724b067816637e570b409d13597023b8d1
Wellington Santos <wellington.vsantos@gmail.com>
2020-01-23 17:56:01 -0300

    classe e form curso join

--- SncMusic/Curso.cs
+++ SncMusic/Curso.cs
@@ -3,10 +3,166 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
-namespace SncMusicClassLab
+namespace SncMusic
 {
-    class Curso
+    public class Curso
     {
+        //atributos
+        private int id;
+        private string nome;
+        private int cargaHoraria;
+        private double valor;
+      
+        //propriedades
+        public int Id { get => id; set => id = value; } //encapsulamento
+        public string Nome { get => nome; set => nome = value; }
+        public int CargaHoraria { get => cargaHoraria; set => cargaHoraria = value; }
+        public double Valor { get => valor; set => valor = value; }
+        //métodos construtores (new Curso())
+        public Curso() 
+        {
+            valor = 0.0;
+        }
+        public Curso(int id, string nome, int cargaHoraria, double valor)
+        {
+            this.id = id;
+            this.nome = nome;
+            this.cargaHoraria = cargaHoraria;
+            this.valor = valor;
+        }
+        public Curso(string nome, int cargaHoraria, double valor)
+        {
+            this.nome = nome;
+            this.cargaHoraria = cargaHoraria;
+            this.valor = valor;
+        }
+        public Curso(string nome, double valor)
+        {
+            this.nome = nome;
+            this.valor = valor;
+        }
+        //métodos da classe
+        public void Inserir() 
+        {
+            try
+            {
+                var comm = Banco.Abrir();
+                comm.CommandText = "insert into tb_curso values(0,@nome, @carga_horaria,@valor)";
+                comm.Parameters.Add("@nome", MySqlDbType.VarChar).Value = Nome;
+                comm.Parameters.Add("@carga_horaria", MySqlDbType.Int32).Value = CargaHoraria;
+                comm.Parameters.Add("@valor", MySqlDbType.Decimal).Value = Valor;
+                comm.ExecuteNonQuery();
+                comm.CommandText = "select @@identity";
+                Id = Convert.ToInt32(comm.ExecuteScalar());
+                Banco.Fechar();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+        public bool Alterar(Curso curso) 
+        {
+            try
+            {
+                var comm = Banco.Abrir();
+                comm.CommandText = "update tb_curso set nome_curso = @nome," +
+                    "carga_horaria_curso = @carga" +
+                    ", valor_curso = @valor where id_curso = @id";
+                comm.Parameters.Add("@id", MySqlDbType.Int32).Value = curso.Id;
+                comm.Parameters.Add("@nome", MySqlDbType.VarChar).Value = curso.Nome;
+                comm.Parameters.Add("@carga", MySqlDbType.Int32).Value = curso.CargaHoraria;
+                comm.Parameters.Add("@valor", MySqlDbType.Decimal).Value = curso.Valor;
+                comm.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public void ConsultarPorId(int _id)
+        {
+            try
+            {
+                var comm = Banco.Abrir();
+                comm.CommandText = "select * from tb_curso where id  =" + _id;
+                var dr = comm.ExecuteReader();
+                while (dr.Read())
+                {
+                    Id = dr.GetInt32(0);
+                    Nome = dr.GetString(1);
+                    CargaHoraria = dr.GetInt32(2);
+                    Valor = Convert.ToDouble(dr.GetDecimal(3));
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+        public List<Curso> ConsultarPorNome(string caracteres) 
+        {
+            List<Curso> lista = new List<Curso>();
+            try
+            {
+                var comm = Banco.Abrir();
+                comm.CommandText = "select * from tb_curso where nome_curso like '%@carac%'";
+                comm.Parameters.Add("@carac", MySqlDbType.VarChar).Value = caracteres;
+                var dr = comm.ExecuteReader();
+                while (dr.Read())
+                {
+                    lista.Add(new Curso(dr.GetInt32(0), dr.GetString(1), dr.GetInt32(2), dr.GetDouble(3)));
+                }
+                return lista;
+            }
+            catch (Exception)
+            {
+                return lista;
+            }
+
+        }
+        public bool AssociarProfessor(int _id_curso, int _id_professor)
+        {
+            try
+            {
+                var comm = Banco.Abrir();
+                comm.CommandText = "insert into tb_professor_curso values(@prof, @curso, default)";
+                comm.Parameters.Add("@prof", MySqlDbType.Int32).Value = _id_professor;
+                comm.Parameters.Add("@curso", MySqlDbType.Int32).Value = _id_curso;
+                comm.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public List<Professor> ConsultarAssociacao(int _id_curso)
+        {
+            List<Professor> lista = new List<Professor>();
+            try
+            {
+                var comm = Banco.Abrir();
+                comm.CommandText = "select p.id_professor, p.nome_professor, p.cpf_professor, p.email_professor " +
+                    "from tb_professor_curso pc inner join tb_professor p " +
+                    "on pc.professor_id_professor = p.id_professor " +
+                    "where pc.curso_id_curso =  "+ _id_curso;
+                var dr = comm.ExecuteReader();
+                while (dr.Read())
+                {
+                    lista.Add(new Professor(dr.GetInt32(0), dr.GetString(1), dr.GetInt32(2), dr.GetDouble(3)));
+                }
+                return lista;
+            }
+            catch (Exception)
+            {
+                return lista;
+            }
+
+        }
     }
 }
--- SncMusic/FrmCurso.Designer.cs
+++ SncMusic/FrmCurso.Designer.cs
@@ -28,12 +28,218 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtCargaHoraria = new System.Windows.Forms.TextBox();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnAssociar = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Location = new System.Drawing.Point(162, 179);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(75, 23);
+            this.btnInserir.TabIndex = 0;
+            this.btnInserir.Text = "&Inserir";
+            this.btnInserir.UseVisualStyleBackColor = true;
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(243, 179);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(75, 23);
+            this.btnConsultar.TabIndex = 1;
+            this.btnConsultar.Text = "&Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(324, 179);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(75, 23);
+            this.btnAlterar.TabIndex = 2;
+            this.btnAlterar.Text = "&Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(133, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(116, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Nome";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(79, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Carga Horária";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(103, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Valor R$";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(162, 53);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(59, 20);
+            this.txtId.TabIndex = 7;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(162, 80);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(274, 20);
+            this.txtNome.TabIndex = 8;
+            // 
+            // txtCargaHoraria
+            // 
+            this.txtCargaHoraria.Location = new System.Drawing.Point(162, 107);
+            this.txtCargaHoraria.Name = "txtCargaHoraria";
+            this.txtCargaHoraria.Size = new System.Drawing.Size(100, 20);
+            this.txtCargaHoraria.TabIndex = 9;
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(162, 134);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(100, 20);
+            this.txtValor.TabIndex = 10;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.btnAssociar);
+            this.groupBox1.Location = new System.Drawing.Point(61, 249);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(485, 150);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Associar Professor";
+            // 
+            // btnAssociar
+            // 
+            this.btnAssociar.Location = new System.Drawing.Point(404, 60);
+            this.btnAssociar.Name = "btnAssociar";
+            this.btnAssociar.Size = new System.Drawing.Size(75, 23);
+            this.btnAssociar.TabIndex = 0;
+            this.btnAssociar.Text = "Associar";
+            this.btnAssociar.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(21, 62);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(191, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(218, 62);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(180, 21);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(21, 43);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Curso";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(215, 43);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Professor";
+            // 
+            // FrmCurso
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FrmCurso";
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.txtCargaHoraria);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.btnInserir);
+            this.Name = "FrmCurso";
+            this.Text = "Cadastro de Cursos";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtCargaHoraria;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnAssociar;
     }
 }
